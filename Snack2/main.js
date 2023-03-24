bf2e8de364589444e8ad13179e13783f903b9633
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-24 12:50:17 +0100

    define const

---
+++ Snack2/main.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti tre argomenti:
+ un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+ La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" */
+
+// definisco le variabili
+let a, b , array;
+// do un valore alle variabili
+[a, b, array] = [1, X, [1,2,4,5,6,7]]
+// la b deve essere massimo uguale alla lunghezza dell'array
+X <= array.lenght
+
+// mi scrivo la funzione..sì certo 
+const newArray = function(a, b ,array){
+  
+}
